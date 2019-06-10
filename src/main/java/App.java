aff8c330ada0bf89338833e1341e31ccbb67a8c8
Marvin Zachary <acerzach@gmail.com>
2019-06-10 11:39:25 +0300

    Assigned a different port for for deployment on heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,8 +10,19 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        staticFileLocation("/public");
-        String layout = "templates/layout.vtl";
+        ProcessBuilder process = new ProcessBuilder();
+           Integer port;
+           if (process.environment().get("PORT") != null) {
+               port = Integer.parseInt(process.environment().get("PORT"));
+           } else {
+               port = 4567;
+           }
+      
+          setPort(port);
+  
+          staticFileLocation("/public");
+          String layout = "templates/layout.vtl";
+  
 
        //BasicConfigurator.configure();
 
